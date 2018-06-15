03236e746772c34bdcf6c90dcb48ae3903e9cce8
Pavel Kozlov <sovvvest@yandex.ru>
2018-06-15 07:55:58 +0400

    Added server config

--- homework11_networks_tunnels/script.sh
+++ homework11_networks_tunnels/script.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-#yum install -y epel-release
-#yum install -y openvpn easy-rsa
-#cp -ar /usr/share/easy-rsa /srv
+#Installing packets
+
+yum install -y epel-release
+yum install -y openvpn easy-rsa
+
+#Creating CA and server keys
+
 cd /usr/share/easy-rsa/3
 echo "yes" | /usr/share/easy-rsa/3/easyrsa init-pki
 echo "openvpnserver" | /usr/share/easy-rsa/3/easyrsa build-ca nopass
@@ -14,3 +18,25 @@ echo "yes" | /usr/share/easy-rsa/3/easyrsa sign-req server server
 cp -ar /usr/share/easy-rsa/3/issued/server.crt /etc/openvpn
 cp -ar /usr/share/easy-rsa/3/pki/private/server.key /etc/openvpn
 cp -ar /usr/share/easy-rsa/3/pki/dh.pem /etc/openvpn
+
+#Generating client certificates
+
+echo "client" | /usr/share/easy-rsa/3/easyrsa gen-req client nopass
+echo "yes" | /usr/share/easy-rsa/3/easyrsa sign-req client client
+cp -ar /usr/share/easy-rsa/3/issued/client.crt /srv
+cp -ar /usr/share/easy-rsa/3/pki/private/client.key /srv
+
+#Setup server config
+
+cp -ar /srv/server.conf /etc/openvpn
+mkdir /etc/openvpn/ccd && mkdir /var/log/openvpn
+echo "iroute 192.168.0.0 255.255.255.0" > /etc/openvpn/ccd/client
+
+#Da zadolbal on!
+
+setenforce 0
+
+#Service start
+
+systemctl start openvpn@server
+systemctl enable openvpn@server
